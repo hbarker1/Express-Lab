cd059c898e7b5500560eb376810c52d533157d23
hannah barker <hannah.m.barker@gmail.com>
2019-05-28 17:39:40 -0400

    added public folder

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 // require the Express module
 const express = require('express'); // express refers to module name, returning ...
 
@@ -17,14 +18,48 @@ app.use("/", cartItems);
 
 
 // define the port
-const port = 2000; //local host 3000
+// const port = 2000; //local host 3000
+const pg = require("pg");
+const pool = new pg.Pool({
+  user: "postgres",
+  password: "Angelofmusic1!",
+  host: "localhost",
+  port: 5432,
+  database: "ExpressShopDB",
+  ssl: false
+});
+
+
+
+app.get("/ShoppingCart", (req, res) => {
+    pool.query("SELECT * FROM ShoppingCart")
+    .then( (results) => {
+        res.send(results.rows);
+    });
+});
+
+app.post("/ShoppingCart", (req, res) => {
+    let data = req.body;
+    pool.query(
+        "INSERT INTO ShoppingCart (product, quantity, price)" + 
+        "values($1::varchar(20), $2::smallint, $3::real)", 
+        [data.product, data.quantity, data.price]
+    )
+    .then( () => {
+        res.send('SUCCESS!!!!!');
+    }) //closing the promise
+});
+
+
 
 
 // run the server
-app.listen(port, () => console.log(`Listening on port: ${port}.`));
+
 //listening 3000 port, excute (call) function
 
-
+app.listen(2000, () => {
+    console.log("JSON Server is running on 2000");
+});
 
 // respond with "Hello Class!" at URI: /cartItems
 //req = request
