4687e274b900a523da6fa54bf04755fac3d78070
hannah barker <hannah.m.barker@gmail.com>
2019-05-29 20:32:32 -0400

    revised cartservice

--- public/CartService.js
+++ public/CartService.js
@@ -1,58 +1,23 @@
-function ExpressService($http, $scope) {
+function ExpressService($http, $q) {
     const service = this;
-    service.key = 'rEKWKvPrW7HpooXYxbjymSfIAOyKcwvvhixHIIWNAYlZeLLzmv'; // Fill in here
-    service._secret = 'OlpuSV27PqFDXswHBLBMPCHuTPh6DfxauvnwNSO5'; // Fill in here
-    service.token = '';
-    /**
-     * Call https://www.reddit.com/r/aww/.json
-     * and set ctrl.feed to be the results
-     */
-    service.getAllItems = () => {
-        // $http stuff goes here
-        return $http({
-            method: "GET",
-            url: "https://www.reddit.com/r/aww/.json"
-            });
-}
+  
 
+    service.getCartData = () => {
+        return $q(function (resolve, reject) {
+            $http.get("/cartItems")
+            .then((response) => {
+                console.log(response.data)
+                resolve(response.data);
+            })
+            .catch((err) => {
+                console.error(err);
+                reject(err);
 
-$scope.enter = (() => {
-    let addTask = $scope.addTask;
-    let completed = $scope.completed;
-    let listItem = $scope.listItem;
-  //adding tasks / filtering  
-        if (!listItem == '' && !addTask == '') {
-            $scope.todos.splice(listItem, 1, {
-                text: addTask,
-                completed: completed
-            });
-        }else {
-            $scope.todos.push({
-                text: addTask,
-                complete: false
-            });
-        };        
-        $scope.addTask = $scope.completed = $scope.listItem = '';  
-}); 
+            })
 
-//complete button to strikethrough list item
-$scope.clear = (() => {
-let todos = $scope.todos;
-$scope.todos = [];
-angular.forEach(todos, function(task) {
-  if (!task.completed) $scope.todos.push(task);
-});
-});
-
-// x button to remove list item
-$scope.removeTask = (() => {
-let lastTask = $scope.todos;
-$scope.todos = [];
-angular.forEach(lastTask, function(x) {
-    if (!x.completed) $scope.todos.push(x);
-});
-});
-}
+        })
+    
+    }
 
 
 
